853b7008b237b1e7ea5b38902d98190d0e009bd9
Jack Learns to Code <woaijack@gmail.com>
2025-04-02 10:49:46 +0800

    update head

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,9 +3,13 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Jack Wiki 杰克百科</span>,
-  head: {
-    faviconGlyph: '✦'
-  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Nextra" />
+      <meta property="og:description" content="The next site builder" />
+    </>
+  ),
   project: {
     link: 'https://github.com/jacklearntech/documentation-starter-kit',
   },
