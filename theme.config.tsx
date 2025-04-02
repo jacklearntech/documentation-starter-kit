836d3b2efe9a64cd579354a777e12c242d569747
Jack Learns to Code <woaijack@gmail.com>
2025-04-02 11:06:27 +0800

    update homepage

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,7 +7,7 @@ const config: DocsThemeConfig = {
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="Nextra" />
+      <meta property="og:title" content="Jack Wiki" />
       <meta property="og:description" content="The next site builder" />
     </>
   ),
