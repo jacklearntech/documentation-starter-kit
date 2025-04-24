9f7a367fdd4f122782ead10253139e6240d0b1d5
Jack Learns to Code <woaijack@gmail.com>
2025-04-24 10:43:54 +0800

    update ig icon

--- theme.config.tsx
+++ theme.config.tsx
@@ -16,6 +16,7 @@ const config: DocsThemeConfig = {
   },
   chat: {
     link: 'https://discord.com',
+    icon: '<i class="fab fa-instagram"></i>',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
